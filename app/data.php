a14c49ed62895d5cf7d987f297111d17d29e6264
Dan Kim <>
2024-03-14 17:39:13 -0400

    Sfx added, bare min functional, basic formatting fixes

--- app/data.php
+++ app/data.php
@@ -1,8 +1,9 @@
 <?php
 $campersDir = "/Users/dank/Documents/SJC/sjc-faces-quiz/faces/campers";
 $staffDir = "/Users/dank/Documents/SJC/sjc-faces-quiz/faces/staff";
-$facesDir = "/Users/dank/Documents/SJC/sjc-faces-quiz/faces/";
+$facesDir = "/Users/dank/Documents/SJC/sjc-faces-quiz/faces";
 $facesDict = [];
+$directory = "";
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
@@ -13,30 +14,31 @@ ini_set('display_errors', 1);
 // For now, campers and staff are combined. Later, will need to be separated so users can choose which set to use.
 function scanAllDir($dir,$parent='') {
     global $facesDict;
+    global $directory;
 
     foreach(scandir($dir) as $filename) {
         if ($filename[0] === '.' || $filename[0] === 'DS_Store') continue; // Skip dots
+        
         $filePath = $dir . '/' . $filename;
         if (is_dir($filePath)) {
             foreach (scanAllDir($filePath,$filename) as $childFilename) {} // Recursive call
         } else {
-            $facesDict[$parent][] = $filePath; // If $filepath leads to an img, add to array for that name.
+            $set = basename(dirname($dir));
+            if ($set === "campers") {
+                $directory = "campers/";
+            } else if ($set === "staff") {
+                $directory = "staff/";
+            }
+            $facesDict[$parent][] = 'faces/'.$directory.$parent.'/'.$filename; // If $filepath leads to an img, add to array for that name.
         }
     }
     return $facesDict;
 }
 
-// Returns an array of people from either campers, staff, or both.
-function getFaceSet() {
-    echo json_encode(scanAllDir($campersDir));
-}
-
-echo $_SERVER['PHP_SELF'];
-
 //if (isset($_GET['set'])) {
     //$set = $_GET['set'];
     $set = 'all';
-    switch ($set) {
+    switch ($set || null) {
         case 'all':
             echo json_encode(scanAllDir($facesDir));
             break;
@@ -47,7 +49,4 @@ echo $_SERVER['PHP_SELF'];
             echo json_encode(scanAllDir($staffDir));
             break;
     }
-// } else {
-//     echo 'no params';
-// }
 ?>
--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,11 @@
 var faces_all = [];
 var faces_working = [];
+var score = 0; // Current score: +1 for first name, +2 for first & last
+var answer = ""; // Name of current person
+var gameOver = false; // Has player gone through all available faces?
 
-document.addEventListener("DOMContentLoaded", gameInit);
+document.getElementById("replay").style.display = "none";
+document.getElementById("submit").disabled = true;
 
 fetch('app/data.php?' + new URLSearchParams({set: 'all'}), {
     method: 'GET',
@@ -13,36 +17,56 @@ fetch('app/data.php?' + new URLSearchParams({set: 'all'}), {
         }
         return response.json();
     })
-    .then(data => (faces_working = data))
+    .then(data => {
+        faces_all = data;
+    })
     .catch(error => {
         console.error('Fetch error:', error);
     });
 
+var nicknamesPath = "nicknames.txt";
+
+// Initialize game with specified time limit, reset score, start with first face 
 function gameInit() {
+    document.getElementById("Popup").style.display = "none"; // Hide popup window
+    document.getElementById("submit").disabled = false;
+    document.getElementById("textinput").value = "";
+    document.getElementById("replay").style.display = "none";
     faces_working = faces_all;
+    gameOver = false;
+    score = 0;
+    document.getElementById("score").innerText = score;
     loadNewFace();
     startTimer(60);
 }
 
+// Show popup window on page load to set preferences and start game.
+function setUp() {
+}
+
 // Choose a random person from the working set and load the image into the image container
 function loadNewFace() {
-    // If working faces array is empty, end game and cancel timer.
-    
-    // Randomly choose face from working faces array
-    var randomIndex = Math.floor(Math.random() * faces_working.length); 
-    var randomFace = faces_working[randomIndex];
+    // If working faces array is empty, end the game.
+    if (Object.keys(faces_working).length === 0) {
+        gameOver = true;
+        gameEnd();
+        return;
+    }
 
-    // Choose random img in folder and set img src
-    var path = "";
+    // Randomly choose face from working faces array
+    var keys = Object.keys(faces_working);
+    var randomIndex = Math.floor(Math.random() * keys.length); 
+    var randomFace = keys[randomIndex];
+
+    answer = randomFace.replace(/_/g, " ");
+    console.log(answer);
+
+    // Choose a random img in folder (if more than one) and set img src
+    var randomImg = Math.floor(Math.random() * faces_working[randomFace].length);
+    var path = faces_working[randomFace][randomImg];
+    document.getElementById("image").src = path;
     
-    // Create new img element and add to html
-    var newImg = document.createElement("img");
-    newImg.src = path;
-    newImg.setAttribute("unselectable", "on");
-    var container = document.getElementById("ImageContainer");
-    container.appendChild(newImg);
-    
-    delete faces_working[randomIndex]; // Remove face so it's not repeated
+    delete faces_working[randomFace]; // Remove face so it's not repeated
 }
 
 // Start a countdown timer for game
@@ -53,24 +77,59 @@ function startTimer(seconds) {
     var gameTimer = setInterval(function() {
         timer.innerHTML = "00:"+timeRemaining;
         timeRemaining -= 1;
-        if (timeRemaining <= 0) {
+        if (timeRemaining <= 0 || gameOver) {
             clearInterval(gameTimer);
             timer.innerHTML = "00:00";
             gameEnd();
+            return;
         }
     }, 1000);
 }
 
 // Check if user's input is correct or not
-function checkAnswer() {
+function checkAnswer(form) {
     //"clean" input of case, minor spelling errors
-    //Add to score if correct
     //Display "correct"/"incorrect" text
+
+    var input = form.inputbox.value.replace(/[^a-zA-Z0-9\s]/g, "").toLowerCase().trim().split(" "); // Remove special characters, converter to lower
+    var correct = answer.toLowerCase().replace(/'/g, "").split(" ");
+
+    // Check if first name matches, if yes then check second name if there is one
+    // Scan the nicknames.txt file, check if the name is there, then load the nickname
+    altName = "";
+
+    if (input[0] === correct[0] || input[0] === altName) {
+        score++;
+        console.log("First name correct!");
+        // Show "correct" text
+        if (input.length > 1 && input[1] === correct[1]) {
+            score++;
+            console.log("Second name correct!");
+        } 
+        // Play correct "ding" sfx
+        var ding = document.getElementById("dingSFX");
+        ding.play();
+    } else {
+        console.log("Incorrect :(");
+    }
+
+    var scoreCard = document.getElementById("score");
+    scoreCard.innerText = score;
+
+    document.getElementById("textinput").value = ""; // Reset input box
     loadNewFace();
 }
 
-// Handles the end of game
+// Handle end of game, lock input, show leaderboard
 function gameEnd() {
-    // Show score, leaderboard
-    // Show restart button
+    document.getElementById("submit").disabled = true;
+    document.getElementById("skip").disabled = true;
+    document.getElementById("replay").style.display = "block";
 }
+
+document.getElementById("quizForm").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevent form submission
+      document.getElementById("submit").click(); // Simulate a click on the submit button
+    }
+});
