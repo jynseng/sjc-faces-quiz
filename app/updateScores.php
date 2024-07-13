5f9c0c91d1652231a0d274c52a57f33ed0e3793b
Dan Kim <>
2024-07-13 18:11:40 -0400

    missed $

--- app/updateScores.php
+++ app/updateScores.php
@@ -2,10 +2,14 @@
 // Accepts POST name and score pair, sort through scores.json and input score in leaderboard (existing scores take higher
 // place). Echo current top ten scores.
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $leaderboard = "/home/u955586838/domains/sjcfacesgame.com/public_html/scores.json";
 $postedData = file_get_contents("php://input");
 
-if (!postedData) {
+if (!$postedData) {
     http_response_code(400);
     die("Error: No data provided");
 }
