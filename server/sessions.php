59cfebbd4d07d9110fde146ee00e8791676f41bc
Dan Kim <>
2024-09-17 21:22:47 -0400

    Update gitignore, update php save path

--- server/sessions.php
+++ server/sessions.php
@@ -26,8 +26,8 @@ session_write_close();
 $timeout = 20; // Seconds after last request before user is taken off active user list
 $sessionTimeout = 60 * 60; // Seconds after last request before session is destroyed (1hr)
 $sleepTime = 100000; // Microsecond delay between loops (.1 seconds)
-//$sessionSavePath = '/opt/alt/php81/var/lib/php/session';
-$sessionSavePath = '/Applications/MAMP/tmp/php';
+$sessionSavePath = '/opt/alt/php81/var/lib/php/session';
+//$sessionSavePath = '/Applications/MAMP/tmp/php';
 if (isset($_SESSION['active_users'])) { $storedActiveUsers = $_SESSION['active_users']; }
 $thisSession = $_SESSION;
 $currentTime = time();
