3584b7532148a8a1791f10150978aed9f51b39bc
Dan Kim <>
2024-09-29 19:30:14 -0400

    Catch websocket errors

--- app/js/user_activity.js
+++ app/js/user_activity.js
@@ -10,37 +10,48 @@ let loggedIn = false;
 let active = true;
 let checkInterval = checkIntervalActive;
 
-let ws = new WebSocket('wss://sjcfacesgame.com/ws/'); // wss for https
+function initWebSocket() {
+    let ws = new WebSocket('wss://sjcfacesgame.com/ws/'); // wss for https
 
-ws.onmessage = function(msg) {
-    activeUsers = JSON.parse(msg.data);
-    loggedIn = true;
-    userList.innerHTML = '';
-    activeUsers.forEach(user => {
-        const li = document.createElement('li');
-        li.textContent = user;
-        userList.appendChild(li);
-    });
-};
+    ws.onerror = function(event) {
+        console.log("WS error: ".event.type);
+        setTimeout(initWebSocket, retryTimeout);
+    }
+
+    ws.onmessage = function(msg) {
+        activeUsers = JSON.parse(msg.data);
+        loggedIn = true;
+        userList.innerHTML = '';
+        activeUsers.forEach(user => {
+            const li = document.createElement('li');
+            li.textContent = user;
+            userList.appendChild(li);
+        });
+    };
+}
 
 function sendUsername(username) {
-    playerName = username;
-    loggedIn = true;
-    console.log(playerName);
-    const message = JSON.stringify({
-        type: 'sign_in', 
-        username: playerName
-    });
-    loginSFX.play();
-    ws.send(message);
+    if (ws.readyState === WebSocket.OPEN) {
+        playerName = username;
+        loggedIn = true;
+        console.log(playerName);
+        const message = JSON.stringify({
+            type: 'sign_in', 
+            username: playerName
+        });
+        loginSFX.play();
+        ws.send(message);
+    }
 }
 
 function sendInactive() {
-    const message = JSON.stringify({
-        type: 'sign_out', 
-        username: playerName
-    });
-    ws.send(message);
+    if (ws.readyState === WebSocket.OPEN) {
+        const message = JSON.stringify({
+            type: 'sign_out', 
+            username: playerName
+        });
+        ws.send(message);
+    }
 }
 
 // Check if user is still active locally every 15 seconds
